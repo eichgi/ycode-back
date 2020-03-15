7f396cc674386de26e9cb98e36f998b7bfd713b9
Hiram Guerrero <hiram.guerrero@itexico.com>
2020-03-15 07:42:38 -0600

    Improving Websites seeder

--- database/seeds/WebsitesSeederTable.php
+++ database/seeds/WebsitesSeederTable.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Website;
+use GuzzleHttp\Client;
 use Illuminate\Database\Seeder;
 
 class WebsitesSeederTable extends Seeder
@@ -12,6 +13,29 @@ class WebsitesSeederTable extends Seeder
      */
     public function run()
     {
-        factory(Website::class, 20)->create();
+        /**
+         * Due to faker word repetition inside the factory this method is not 100% accurate
+         * since you might have to run it several times until no uniqueness collision happens.
+         */
+        //factory(Website::class, 20)->create();
+
+        /**
+         * Unlike the previous method, this will consume an API for words gathering
+         */
+        try {
+            $client = new Client();
+            $response = $client->get('https://random-word-api.herokuapp.com/word?number=100');
+            $sites = json_decode($response->getBody());
+
+            collect($sites)->each(function ($site) {
+                Website::create([
+                    'name' => $site,
+                    'url' => "{$site}.example.com",
+                    'created_at' => now()->addMinutes(rand(1, 60)),
+                ]);
+            });
+        } catch (Exception $exception) {
+            $this->info($exception->getMessage());
+        }
     }
 }
